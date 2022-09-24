f729c05b704dee118d6ea91194eb19762d54de73
Nick To <admin@gobox-t.com>
2022-09-24 14:14:20 +0800

    GoBox Common Libraries

--- core/src/main/java/com/gobox/core/ui/GoButton.kt
+++ core/src/main/java/com/gobox/core/ui/GoButton.kt
@@ -1,31 +1,55 @@
 package com.gobox.core.ui
 
 import android.content.Context
+import android.content.res.ColorStateList
 import android.graphics.Canvas
 import android.graphics.ColorFilter
 import android.graphics.PorterDuff
 import android.graphics.PorterDuffColorFilter
 import android.graphics.drawable.Drawable
+import android.graphics.drawable.GradientDrawable
 import android.util.AttributeSet
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.appcompat.widget.AppCompatButton
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.toColor
 import androidx.core.view.marginTop
 import com.gobox.core.R
 
 
 class GoButton: AppCompatButton {
-    private var isActive: Boolean = true
+    private var active: Boolean = true
+    private var radius: Int = 8
+    private var activeBackgroundColorResId: Int = R.color.button_active
+    private var activeTextColorResId: Int = R.color.button_text_active
+    private var inactiveBackgroundColorResId: Int = R.color.button_inactive
+    private var inactiveTextColorResId: Int = R.color.button_text_inactive
 
     constructor(ctx: Context): super(ctx) {
+        refreshButton()
     }
 
     constructor(ctx: Context, attrs: AttributeSet): super(ctx, attrs) {
         val myAttrs = ctx.obtainStyledAttributes(attrs, R.styleable.GoButton)
         for (i in 0 until myAttrs.indexCount) {
             when (val attr = myAttrs.getIndex(i)) {
-                R.styleable.GoButton_isActive -> {
-                    isActive = myAttrs.getBoolean(attr, true)
+                R.styleable.GoButton_go_active -> {
+                    active = myAttrs.getBoolean(attr, true)
+                }
+                R.styleable.GoButton_go_radius -> {
+                    radius = myAttrs.getInt(attr, 8)
+                }
+                R.styleable.GoButton_go_active_background_color -> {
+                    activeBackgroundColorResId = myAttrs.getResourceId(attr, 0)
+                }
+                R.styleable.GoButton_go_active_text_color -> {
+                    activeTextColorResId = myAttrs.getResourceId(attr, 0)
+                }
+                R.styleable.GoButton_go_inactive_background_color -> {
+                    inactiveBackgroundColorResId = myAttrs.getResourceId(attr, 0)
+                }
+                R.styleable.GoButton_go_inactive_text_color -> {
+                    inactiveTextColorResId = myAttrs.getResourceId(attr, 0)
                 }
             }
         }
@@ -33,7 +57,9 @@ class GoButton: AppCompatButton {
         myAttrs.recycle()
     }
 
-    constructor(ctx: Context, attrs: AttributeSet, defStyle: Int): super(ctx, attrs, defStyle)
+    constructor(ctx: Context, attrs: AttributeSet, defStyle: Int): super(ctx, attrs, defStyle) {
+        refreshButton()
+    }
 
     override fun onDraw(canvas: Canvas?) {
         if (compoundDrawables[0]!=null) {
@@ -41,14 +67,14 @@ class GoButton: AppCompatButton {
             var drawableWidth = 0
             if (compoundDrawables[0]!=null) {
                 // add left drawable width with default padding 2
-                drawableWidth += compoundDrawables[0].intrinsicWidth + 2
+                drawableWidth += compoundDrawables[0].intrinsicWidth
             }
             if (compoundDrawables[2]!=null) {
                 // add right drawable width with default padding 2
-                drawableWidth += compoundDrawables[2].intrinsicWidth + 2
+                drawableWidth += compoundDrawables[2].intrinsicWidth
             }
 
-            val textWidth = (paint.measureText(text.toString()) + textSize * 2).toInt()
+            val textWidth = (paint.measureText(text.toString()) + textSize).toInt()
             val autoPadding: Int = (width - drawableWidth - textWidth) / 2
             setPadding(autoPadding, 0, autoPadding, 0)
         }
@@ -56,17 +82,33 @@ class GoButton: AppCompatButton {
     }
 
     private fun refreshButton() {
-        if (isActive) {
-            background = AppCompatResources.getDrawable(context, R.drawable.button_active)
-            setTextColor(context.getColor(R.color.button_text_active))
+        if (active) {
+            // 1. Set background color
+            val shape = GradientDrawable()
+            shape.cornerRadius = radius.toFloat()
+            shape.setColor(context.getColor(activeBackgroundColorResId))
+            background = shape
+
+            // 2. Set text color
+            setTextColor(context.getColor(activeTextColorResId))
+
+            // 3. Set image color
             compoundDrawables.forEach {
                 if (it != null) {
                     it.colorFilter = PorterDuffColorFilter(ContextCompat.getColor(context, R.color.button_text_active), PorterDuff.Mode.SRC_IN)
                 }
             }
         } else {
-            background = AppCompatResources.getDrawable(context, R.drawable.button_inactive)
-            setTextColor(context.getColor(R.color.button_text_inactive))
+            // 1. Set background color
+            val shape = GradientDrawable()
+            shape.cornerRadius = radius.toFloat()
+            shape.setColor(context.getColor(inactiveBackgroundColorResId))
+            background = shape
+
+            // 2. Set text color
+            setTextColor(context.getColor(inactiveTextColorResId))
+
+            // 3. Set image color
             compoundDrawables.forEach {
                 if (it != null) {
                     it.colorFilter = PorterDuffColorFilter(ContextCompat.getColor(context, R.color.button_text_inactive), PorterDuff.Mode.SRC_IN)
@@ -75,12 +117,12 @@ class GoButton: AppCompatButton {
         }
     }
 
-    fun setActive(active: Boolean) {
-        isActive = active
+    fun setActive(act: Boolean) {
+        active = act
         refreshButton()
     }
 
     fun getActive(): Boolean {
-        return isActive
+        return active
     }
 }
